03af81fa2167f73c3e602933bbc6f24d2c1fd54a
tiago_muzikante <tiagommuzikante@hotmail.com>
2025-08-14 16:13:24 -0300

    Initial commit

---
+++ src/test/java/space/vendas/sistema/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package space.vendas.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
