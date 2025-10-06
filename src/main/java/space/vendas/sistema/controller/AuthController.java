100b1ed78265cad6ef2174fc57cca9e0544e3430
tiago_muzikante <tiagommuzikante@hotmail.com>
2025-10-06 17:23:54 -0300

    fix auth controller

--- src/main/java/space/vendas/sistema/controller/AuthController.java
+++ src/main/java/space/vendas/sistema/controller/AuthController.java
@@ -1,30 +1,20 @@
 package space.vendas.sistema.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import space.vendas.sistema.dto.auth.AuthDTO;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
-  @GetMapping("/login")
-  public ResponseEntity<Void> login(){
-    return ResponseEntity.noContent().build();
-  }
+  @PostMapping("/login")
+  public void login(@RequestBody AuthDTO dto){
 
-  @GetMapping("/ping")
-  public ResponseEntity<String> ping(){
-    return ResponseEntity.ok("pong");
-  }
-
-  @PostMapping("/nome")
-  public ResponseEntity<String> nome(){
-    return ResponseEntity.ok("nome");
   }
 
 }
